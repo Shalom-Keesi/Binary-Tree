c2900995deae7e0c61075c94f7b262f64691fcba
Shalom-Keesi <shalomkeesi@gmail.com>
2024-04-01 15:53:42 +0300

    added files

---
+++ binary.py
@@ -0,0 +1,38 @@
+class binaryTree:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def postOrderTraversal(node):
+    if node is None:
+        return
+    postOrderTraversal(node.left)
+    postOrderTraversal(node.right)
+    print(node.data, end=", ")
+
+
+
+
+root = binaryTree("Furniture")
+nodeA = binaryTree("Couch")
+nodeB = binaryTree("Table")
+nodeC = binaryTree("Stool")
+nodeD = binaryTree("Drawer")
+nodeE = binaryTree("Seat")
+nodeF = binaryTree("Pillow")
+
+root.left = nodeA
+root.right = nodeB
+
+nodeA.left = nodeC
+nodeA.right = nodeD
+
+nodeB.left = nodeE
+nodeB.right = nodeF
+
+
+postOrderTraversal(root)
+
+
